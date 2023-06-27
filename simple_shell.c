e04d6f726587510467ebc0f8bb7115a3143127e8
wanjirumwai <wanjiru_mwai@gmail.com>
2023-06-27 14:42:00 -0700

    simple_shell.c

--- simple_shell.c
+++ simple_shell.c
@@ -19,11 +19,12 @@ void handle_signal(int signal);
 {
 
 void handle_signal(int signal){
-    if (signal == SIGINT) {
-        printf("\n");
-        printf("simple_shell$ ");
-        fflush(stdout);
-    }
+	if (signal == SIGINT) {
+		printf("\n");
+		printf("simple_shell$ ");
+		fflush(stdout);
+	}
+
 /**
  * main - program entry point
  *
@@ -33,40 +34,38 @@ void handle_signal(int signal){
 int main(void) 
 
 {
-    char c[1024];
-    int i;
-
-    signal(SIGINT, handle_signal); 
-
-    while (1) {
-        printf("simple_shell$ ");
-        if (fgets(c, sizeof(c), stdin) == NULL) {
-            printf("\n");
-            break;
-        }
-
-        c[strcspn(c, "\n")] = '\0';
-
-        if (strlen(c) == 0) {
-            continue;
-        }
-
-        pid_t pid = fork();
-
-        if (pid < 0) {
-            perror("Fork error");
-            exit(EXIT_FAILURE);
-        } else if (pid == 0) {
+	char c[1024]
+		int i;
+	
+	signal(SIGINT, handle_signal); 
+	
+	while (1) {
+		printf("simple_shell$ ");
+		if (fgets(c, sizeof(c), stdin) == NULL) {
+			printf("\n");
+			break;
+		}
+		
+		c[strcspn(c, "\n")] = '\0';
+		
+		if (strlen(c) == 0) {
+			continue;
+		}
+		
+		pid_t pid = fork();
+		
+		if (pid < 0)
+			perror("Fork error");
+		exit(EXIT_FAILURE);
+	} else if (pid == 0) {
             
-            char *args[] = {c, NULL};
-            if (execve(c, args, environ) == -1) {
-                perror("Execution error of command");
-                exit(EXIT_FAILURE);
-            }
-        } else {
-            waitpid(pid, &i, 0);
-        }
-    }
-
-    return 0;
+		char *args[] = {c, NULL};
+		if (execve(c, args, environ) == -1) {
+			perror("Execution error of command");
+			exit(EXIT_FAILURE);
+		}
+	} else {
+		waitpid(pid, &i, 0);
+	}
+	return 0;
 }
