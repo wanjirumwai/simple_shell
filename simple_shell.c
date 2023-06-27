38b16aa3f9d8bdccdd8d989ba61af50ee3100ee4
Anne Musau <amusau4@gmail.com>
2023-06-28 00:37:43 +0300

    Update simple_shell.c

--- simple_shell.c
+++ simple_shell.c
@@ -6,67 +6,72 @@
 #include <signal.h>
 #include <sys/types.h>
 
-/**
- * Handle_signal - This Function informs the compiler of its existence
- * @int - Pointer to the integer value
- *
- * Return - 0 on success
- */
+#define STDIN stdin
 
-extern char **environ;
-
-void handle_signal(int signal);
-{
-
-void handle_signal(int signal){
-    if (signal == SIGINT) {
-        printf("\n");
-        printf("simple_shell$ ");
-        fflush(stdout);
-    }
 /**
  * main - program entry point
+ *Handle_signal - This Function informs the compiler of its existence
+ *@int - Pointer to the integer value
  *
  * Return - Always 0 success.
  */
 
-int main(void) 
+extern char **environ;
+void handle_signal(int signal);
 
 {
-    char c[1024];
-    int i;
+void handle_signal(int signal)
+{
+if (signal == SIGINT) 
+{
+printf("\n");
+printf("simple_shell$ ");
+fflush(stdout);
+}
 
-    signal(SIGINT, handle_signal); 
+int main(void) 
+{
+char c[1024];
+int i;
 
-    while (1) {
-        printf("simple_shell$ ");
-        if (fgets(c, sizeof(c), stdin) == NULL) {
-            printf("\n");
-            break;
-        }
+signal(SIGINT, handle_signal); 
 
-        c[strcspn(c, "\n")] = '\0';
+while (1) {
+printf("simple_shell$ ");
+if (fgets(c, sizeof(c), STDIN) == NULL) 
+{
+printf("\n");
+break;
+}
 
-        if (strlen(c) == 0) {
-            continue;
-        }
+c[strcspn(c, "\n")] = '\0';
 
-        pid_t pid = fork();
+if (strlen(c) == 0) 
+{
+continue;
+}
 
-        if (pid < 0) {
-            perror("Fork error");
-            exit(EXIT_FAILURE);
-        } else if (pid == 0) {
-            
-            char *args[] = {c, NULL};
-            if (execve(c, args, environ) == -1) {
-                perror("Execution error of command");
-                exit(EXIT_FAILURE);
-            }
-        } else {
-            waitpid(pid, &i, 0);
-        }
-    }
+pid_t pid = fork();
 
-    return 0;
+if (pid < 0) 
+{
+perror("Fork error");
+exit(EXIT_FAILURE);
+} 
+else if (pid == 0) 
+{
+           
+char *args[] = {c, NULL};
+if (execve(c, args, STDIN) == -1) 
+{
+perror("Execution error of command");
+exit(EXIT_FAILURE);
+}
+} else 
+{
+waitpid(pid, &i, 0);
+}
+}
+
+return 0;
 }
